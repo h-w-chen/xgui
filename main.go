243450f66c1bd9c7fdff1c780748fbd1fe805d9a
Howell C <xyz>
2019-04-12 22:06:07 -0700

    draw Y in rolling window style

--- main.go
+++ main.go
@@ -20,10 +20,13 @@ var (
 	width  = 400
 	height = 600
 	co     = color.RGBA{0, 200, 0, 0}
+	bg     = xgraphics.BGRA{R: 0x0, G: 0x0, B: 0x0, A: 0xff}
 
-	ticker     = time.NewTicker(50 * time.Millisecond)
-	value      = 0
-	rightmostX = 0
+	ticker = time.NewTicker(20 * time.Millisecond)
+	value  = 0
+	x      = 0
+	boundX = width - 1
+	boundY = height - 1
 )
 
 // type of function to get next Y value to draw
@@ -37,15 +40,25 @@ func getYOnTicker() int {
 }
 
 func drawY(y int, canvas *xgraphics.Image, winID xproto.Window) {
-	if y > height {
-		y = height
+	if y > boundY {
+		y = boundY
 	}
 
-	canvas.Set(rightmostX, y, co)
+	// if the current x is beyond the (right) drawing boundary (should be by 1 only),
+	// left shift the canvas by 1, in order to make room for the current point
+	if x > boundX { // MUST x == boundX + 1
+		if x-boundX != 1 {
+			log.Fatalf("internal error: x=%d, boundX=%d, distance is not 1\n", x, boundX)
+		}
+		leftShiftCanvasByX(canvas, 1)
+		x = boundX
+	}
+
+	canvas.Set(x, y, co)
 	canvas.XDraw()
 	canvas.XPaint(winID)
 
-	rightmostX++
+	x++
 }
 
 func main() {
@@ -70,4 +83,13 @@ func main() {
 	xevent.Main(X)
 
 	fmt.Println("exited")
+}
+
+func leftShiftCanvasByX(canvas *xgraphics.Image, distance int) {
+	canvas.For(func(x, y int) xgraphics.BGRA {
+		//if x >= canvas.Rect.Dx()-distance {
+		//	return bg
+		//}
+		return canvas.At(x+distance, y).(xgraphics.BGRA)
+	})
 }
